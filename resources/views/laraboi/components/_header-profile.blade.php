ff8b9817f2708167d8d42795182de53b9b6709cc
Nico Audy <nicoaudy1994@gmail.com>
2019-11-30 10:26:11 +0700

    display user avatar

--- resources/views/laraboi/components/_header-profile.blade.php
+++ resources/views/laraboi/components/_header-profile.blade.php
@@ -1,10 +1,20 @@
 <div class="dropdown dropdown-profile">
      <a href="" class="dropdown-link" data-toggle="dropdown" data-display="static">
-          <div class="avatar avatar-sm"><img src="https://via.placeholder.com/500" class="rounded-circle" alt=""></div>
+          <div class="avatar avatar-sm">
+               @if(auth()->user()->getFirstMedia())
+               <img src="{{ asset('images'. auth()->user()->getFirstMediaUrl()) }}" alt="" class="rounded-circle" style="height: 35px; width: 35px">
+               @else
+               <img src="https://via.placeholder.com/500" class="rounded-circle" alt="">
+               @endif
+          </div>
      </a>
      <div class="dropdown-menu dropdown-menu-right tx-13">
           <div class="avatar avatar-lg mg-b-15">
+               @if(auth()->user()->getFirstMedia())
+               <img src="{{ asset('images'. auth()->user()->getFirstMediaUrl()) }}" alt="" class="rounded-circle" style="height: 35px; width: 35px">
+               @else
                <img src="https://via.placeholder.com/500" class="rounded-circle" alt="">
+               @endif
           </div>
           <h6 class="tx-semibold mg-b-5">{{ strtoupper(auth()->user()->name) }}</h6>
           <p class="mg-b-25 tx-12 tx-color-03">Administrator</p>
@@ -19,8 +29,7 @@
 
           <div class="dropdown-divider"></div>
 
-          <a class="dropdown-item" href="{{ route('logout') }}"
-               onclick="event.preventDefault(); document.getElementById('logout-form').submit();">
+          <a class="dropdown-item" href="{{ route('logout') }}" onclick="event.preventDefault(); document.getElementById('logout-form').submit();">
                <i data-feather="log-out"></i> {{ __('Logout') }}
           </a>
           <form id="logout-form" action="{{ route('logout') }}" method="POST" style="display: none;">
